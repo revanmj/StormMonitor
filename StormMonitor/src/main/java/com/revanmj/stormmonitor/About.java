94def9553ff4c5f82e7ed7ac3ee20039a29ea8bc
revanmj <revanmj@live.com>
2013-12-28 10:54:18 +0100

    Poprawki

--- StormMonitor/src/main/java/com/revanmj/stormmonitor/About.java
+++ StormMonitor/src/main/java/com/revanmj/stormmonitor/About.java
@@ -33,17 +33,12 @@ public class About extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.about, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                //NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
         return super.onOptionsItemSelected(item);
     }
     
