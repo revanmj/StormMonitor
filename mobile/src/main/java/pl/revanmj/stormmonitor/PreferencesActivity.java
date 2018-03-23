2de037c8e7f0e2041160679ccd9ff5805fd6d3b2
Michał Jakubowski <revanmj@gmail.com>
2018-03-23 15:41:12 +0100

    Use preferences from support library

--- mobile/src/main/java/pl/revanmj/stormmonitor/PreferencesActivity.java
+++ mobile/src/main/java/pl/revanmj/stormmonitor/PreferencesActivity.java
@@ -1,15 +1,16 @@
 package pl.revanmj.stormmonitor;
 
-
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.MenuItem;
 
 import pl.revanmj.stormmonitor.data.SharedSettings;
-import pl.revanmj.stormmonitor.logic.AppCompatPreferenceActivity;
 import pl.revanmj.stormmonitor.logic.StormJob;
 import pl.revanmj.stormmonitor.logic.Utils;
 
@@ -17,27 +18,13 @@ import pl.revanmj.stormmonitor.logic.Utils;
  * Created by revanmj on 13.11.2016.
  */
 
-public class PreferencesActivity extends AppCompatPreferenceActivity {
+public class PreferencesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-
-        final ListPreference syncPeriod = (ListPreference) findPreference(SharedSettings.SYNC_PERIOD);
-        syncPeriod.setOnPreferenceChangeListener((preference, newValue) -> {
-            Integer period = Integer.parseInt((String) newValue);
-            StormJob.scheduleJob(period);
-            return true;
-        });
-
-        int[] widgetIDs = AppWidgetManager.getInstance(this.getApplicationContext())
-                .getAppWidgetIds(new ComponentName(this.getApplicationContext(), CitiesWidget.class));
-        if (widgetIDs.length < 1) {
-            syncPeriod.setEnabled(false);
-        }
-
-        Preference appVersion = findPreference("app_version");
-        appVersion.setTitle(getString(R.string.settings_version, Utils.APP_VERSION));
+        MainPreferenceFragment mainFragment = new MainPreferenceFragment();
+        mainFragment.setContext(this);
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, mainFragment).commit();
     }
 
     @Override
@@ -51,4 +38,32 @@ public class PreferencesActivity extends AppCompatPreferenceActivity {
         return true;
     }
 
+    public static class MainPreferenceFragment extends PreferenceFragmentCompat {
+        private Context mContext;
+
+        public void setContext(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.preferences);
+
+            final ListPreference syncPeriod = (ListPreference) findPreference(SharedSettings.SYNC_PERIOD);
+            syncPeriod.setOnPreferenceChangeListener((preference, newValue) -> {
+                Integer period = Integer.parseInt((String) newValue);
+                StormJob.scheduleJob(period);
+                return true;
+            });
+
+            int[] widgetIDs = AppWidgetManager.getInstance(mContext)
+                    .getAppWidgetIds(new ComponentName(mContext, CitiesWidget.class));
+            if (widgetIDs.length < 1) {
+                syncPeriod.setEnabled(false);
+            }
+
+            Preference appVersion = findPreference("app_version");
+            appVersion.setTitle(getString(R.string.settings_version, Utils.APP_VERSION));
+        }
+    }
 }
